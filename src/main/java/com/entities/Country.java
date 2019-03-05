804e138da4eb973eb9ba1a7caa2917a9b0ff1d7a
mikbac <mbachorz1997@gmail.com>
2019-03-06 00:57:12 +0100

    add lombok

--- src/main/java/com/entities/Country.java
+++ src/main/java/com/entities/Country.java
@@ -1,11 +1,16 @@
 package com.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "country")
 public class Country {
 
     @Id
@@ -13,50 +18,5 @@ public class Country {
     private BigDecimal tax;
     private BigDecimal fixedCosts;
     private String currencyCode;
-
-
-    public Country(String countryCode, BigDecimal tax, BigDecimal fixedCosts, String currencyCode) {
-        this.countryCode = countryCode;
-        this.tax = tax;
-        this.fixedCosts = fixedCosts;
-        this.currencyCode = currencyCode;
-    }
-
-    public Country() {
-
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public BigDecimal getTax() {
-        return tax;
-    }
-
-    public void setTax(BigDecimal tax) {
-        this.tax = tax;
-    }
-
-    public BigDecimal getFixedCosts() {
-        return fixedCosts;
-    }
-
-    public void setFixedCosts(BigDecimal fixedCosts) {
-        this.fixedCosts = fixedCosts;
-    }
-
-    public String getCurrencyCode() {
-        return currencyCode;
-    }
-
-    public void setCurrencyCode(String currencyCode) {
-        this.currencyCode = currencyCode;
-    }
-
 
 }
