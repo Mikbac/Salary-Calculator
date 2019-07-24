3e0aa4bfa2030516b2d16ab2312d9bbd0b1041da
mikbac <mbachorz1997@gmail.com>
2019-07-24 16:14:11 +0200

    updated code

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
+/**
+ * Created by MikBac on 2018
+ */
+
 INSERT INTO country(country_code, currency_code, fixed_costs, tax) VALUES ('UK', 'GBP', 600, 25);
 INSERT INTO country(country_code, currency_code, fixed_costs, tax) VALUES ('DE', 'EUR', 800, 20);
 INSERT INTO country(country_code, currency_code, fixed_costs, tax) VALUES ('PL', 'PLN', 1200, 19);
