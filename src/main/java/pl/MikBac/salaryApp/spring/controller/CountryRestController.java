104c149d95a3bef73b4df5e8353f2615826f190d
MikBac <mbachorz1997@gmail.com>
2019-08-03 23:52:03 +0200

    updated imports

--- src/main/java/pl/MikBac/salaryApp/spring/controller/CountryRestController.java
+++ src/main/java/pl/MikBac/salaryApp/spring/controller/CountryRestController.java
@@ -6,7 +6,13 @@ package pl.MikBac.salaryApp.spring.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.MikBac.salaryApp.constants.WebConstants.Mapping;
 import pl.MikBac.salaryApp.model.CountryModel;
 import pl.MikBac.salaryApp.spring.facade.CountryFacade;
