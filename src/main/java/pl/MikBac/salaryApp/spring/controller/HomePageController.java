04f1e6091cba60c8e5229dc25359c848d08a33ee
mikbac <mbachorz1997@gmail.com>
2019-09-04 00:19:06 +0200

    add languages

--- src/main/java/pl/MikBac/salaryApp/spring/controller/HomePageController.java
+++ src/main/java/pl/MikBac/salaryApp/spring/controller/HomePageController.java
@@ -7,6 +7,7 @@ package pl.MikBac.salaryApp.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.MikBac.salaryApp.constants.AttributeConstants.ModelAttributes.HomePage;
 import pl.MikBac.salaryApp.constants.WebConstants.Mapping;
@@ -14,6 +15,7 @@ import pl.MikBac.salaryApp.constants.WebConstants.Views;
 import pl.MikBac.salaryApp.spring.facade.HomePageFacade;
 
 import javax.annotation.Resource;
+import java.util.Locale;
 
 @Controller
 @RequestMapping(Mapping.ROOT)
@@ -22,9 +24,16 @@ public class HomePageController {
     @Resource
     private HomePageFacade homePageFacade;
 
-    @GetMapping(value = "")
-    public String get(final Model model) {
+    @GetMapping
+    public String get(final Locale locale, final Model model) {
         model.addAttribute(HomePage.COUNTRIES, homePageFacade.getCoutries());
         return Views.MAIN_PAGE;
     }
+
+    @PostMapping
+    public String setLanguage(final Locale locale, final Model model) {
+        model.addAttribute(HomePage.COUNTRIES, homePageFacade.getCoutries());
+        return Views.MAIN_PAGE;
+    }
+
 }
