3620ce752017bb3cebd65315f47b56624b0d9dc9
mikbac <mbachorz1997@gmail.com>
2019-10-07 22:36:40 +0200

    added js pattern

--- src/main/resources/static/js/salary.js
+++ src/main/resources/static/js/salary.js
@@ -2,19 +2,23 @@
  * Created by MikBac on 2018
  */
 
+var APP = APP || {};
 
-var app = angular.module('myApp', []);
+APP.calculator = (function () {
 
+    var app = angular.module('myApp', []);
 
-app.controller('myCtrl', function ($scope, $http) {
-    $scope.content = "0";
-    $scope.calculate = function (country, value) {
-        $http.get("/salary-calculator/salary/pln/?countryCode=" + country + "&salary=" + value, {responseType: 'text'})
-            .then(function successCallback(response) {
-                    $scope.content = response.data;
-                }, function errorCallback(response) {
-                    $scope.content = "error";
-                }
-            );
-    };
-});
+    app.controller('myCtrl', function ($scope, $http) {
+        $scope.content = "0";
+        $scope.calculate = function (country, value) {
+            $http.get("/salary-calculator/salary/pln/?countryCode=" + country + "&salary=" + value, {responseType: 'text'})
+                .then(function successCallback(response) {
+                        $scope.content = response.data;
+                    }, function errorCallback(response) {
+                        $scope.content = "error";
+                    }
+                );
+        };
+    });
+
+})();
