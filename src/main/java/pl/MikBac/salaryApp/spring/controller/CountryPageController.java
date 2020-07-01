02a967292fe5038b9f6229296a244805853dc3ba
mikbac <mbachorz1997@gmail.com>
2020-07-01 11:21:37 +0200

    added swagger docs

--- src/main/java/pl/MikBac/salaryApp/spring/controller/CountryPageController.java
+++ src/main/java/pl/MikBac/salaryApp/spring/controller/CountryPageController.java
@@ -4,6 +4,9 @@
 
 package pl.MikBac.salaryApp.spring.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -32,24 +35,40 @@ import java.util.List;
 public class CountryPageController {
 
     @Resource
-    CountryFacade countryFacade;
+    private CountryFacade countryFacade;
 
+    @ApiOperation(value = "Get a list of all available countries.")
     @GetMapping(value = Mapping.COUNTRIES, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CountryData> allCountries() {
+    public List<CountryData> getCountries() {
         log.info("Getting all countries");
         return countryFacade.getAllCountries();
+    }
+
+    @ApiOperation(value = "Calculate the salary.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "countryCode",
+                    value = "Country code.",
+                    required = true,
+                    dataType = "string",
+                    paramType = "RequestParam",
+                    defaultValue = "UK"),
+            @ApiImplicitParam(name = "salary",
+                    value = "User hourly rate.",
+                    required = true,
+                    dataType = "string",
+                    paramType = "RequestParam",
+                    defaultValue = "99")
+    })
+    @GetMapping(value = Mapping.CALCULATOR, produces = {MediaType.ALL_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public BigDecimal getSalary(@RequestParam(value = "countryCode", required = true) String countryCode, @RequestParam(value = "salary", required = true) String salaryFromClient) {
+        log.info("Counting salary for country code: {} and salary from client: {}", () -> countryCode, () -> salaryFromClient);
+        return countryFacade.calculateSalary(countryCode, salaryFromClient);
     }
 
     @PostMapping(value = Mapping.COUNTRY)
     public ResponseEntity<CountryModel> addCountry(@RequestBody @Valid @NotNull CountryModel country) {
         log.info("Adding new country: {}", () -> country);
         return countryFacade.addCountry(country);
-    }
-
-    @GetMapping(value = Mapping.CALCULATOR, produces = {MediaType.ALL_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public BigDecimal getSalary(@RequestParam(value = "countryCode", required = true) String countryCode, @RequestParam(value = "salary", required = true) String salaryFromClient) {
-        log.info("Counting salary for country code: {} and salary from client: {}", () -> countryCode, () -> salaryFromClient);
-        return countryFacade.calculateSalary(countryCode, salaryFromClient);
     }
 
 }
