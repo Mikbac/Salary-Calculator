198df0b91b5b846a0e59563b16de403cc4fac083
MikBac <mbachorz1997@gmail.com>
2021-09-03 12:47:54 +0200

    update HomePageController

--- src/main/java/pl/mikbac/salaryapp/spring/controller/HomePageController.java
+++ src/main/java/pl/mikbac/salaryapp/spring/controller/HomePageController.java
@@ -3,14 +3,17 @@ package pl.mikbac.salaryapp.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.mikbac.salaryapp.constants.AttributeConstants.ModelAttributes.HomePage;
 import pl.mikbac.salaryapp.constants.WebConstants.Mapping;
 import pl.mikbac.salaryapp.constants.WebConstants.Views;
+import pl.mikbac.salaryapp.data.impl.CountryData;
 import pl.mikbac.salaryapp.spring.facade.CountryFacade;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by MikBac on 2018
@@ -25,20 +28,17 @@ public class HomePageController {
 
     @GetMapping
     public String get(final Model model) {
-        populateModel(model);
-
         return Views.MAIN_PAGE;
     }
 
     @PostMapping
     public String setLanguage(final Model model) {
-        populateModel(model);
-
         return Views.MAIN_PAGE;
     }
 
-    private void populateModel(final Model model) {
-        model.addAttribute(HomePage.COUNTRIES, countryFacade.getAllCountries());
+    @ModelAttribute(HomePage.COUNTRIES)
+    private List<CountryData> populateModel() {
+        return countryFacade.getAllCountries();
     }
 
 }
